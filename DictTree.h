05d09412dce0f296dbb564e49043520b9968e9ab
Rijutady <rijutady@gmail.com>
2019-10-13 22:10:58 -0700

    Add files via upload

--- DictTree.h
+++ DictTree.h
@@ -1,118 +1,125 @@
 #pragma once
 
-using namespace std;
+//using namespace std;
 
 template <class KeyType, class ItemType>
 struct TreeNode
 {
-	// Your implementation...
-	KeyType key;
-	ItemType item;
-	TreeNode<KeyType, ItemType>* left;
-	TreeNode<KeyType, ItemType>* right;
+    // Your implementation...
+    KeyType key;
+    ItemType item;
+    TreeNode<KeyType, ItemType>* left;
+    TreeNode<KeyType, ItemType>* right;
 };
 
 template <class KeyType, class ItemType>
 class DictTree
 {
 private:
-	TreeNode<KeyType, ItemType>* root;
-	TreeNode<KeyType, ItemType>* removeFromSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key, bool& success);
-	TreeNode<KeyType, ItemType>* removeNode(TreeNode<KeyType, ItemType>* node);
-	TreeNode<KeyType, ItemType>* removeLeftmostNode(TreeNode<KeyType, ItemType>* node, TreeNode<KeyType, ItemType>* successor);
-	TreeNode<KeyType, ItemType>* containedInSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key);
-	void preorder(void visit(ItemType&), TreeNode<KeyType, ItemType>* treePtr) const;
-	int numNodes;
+    TreeNode<KeyType, ItemType>* root;
+    TreeNode<KeyType, ItemType>* removeFromSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key, bool& success);
+    TreeNode<KeyType, ItemType>* removeNode(TreeNode<KeyType, ItemType>* node);
+    TreeNode<KeyType, ItemType>* removeLeftmostNode(TreeNode<KeyType, ItemType>* node, TreeNode<KeyType, ItemType>* successor);
+    TreeNode<KeyType, ItemType>* containedInSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key);
+    void preorder(void visit(ItemType&), TreeNode<KeyType, ItemType>* treePtr) const;
+    int numNodes;
 public:
-	DictTree();
-	void add(KeyType newKey, ItemType newItem);
-	bool remove(KeyType newKeys);
-	bool contains(KeyType key);
-	ItemType getItem(KeyType key);
-	int size();
-	void traverse(void visit(ItemType&)) const;
+    DictTree();
+    void add(KeyType newKey, ItemType newItem);
+    bool remove(KeyType newKeys);
+    bool contains(KeyType key);
+    ItemType getItem(KeyType key);
+    int size();
+    void traverse(void visit(ItemType&)) const;
 };
 
 
 template <class KeyType, class ItemType>
 DictTree<KeyType, ItemType>::DictTree()
 {
-	// Set up the root and the initial number of nodes to 0
-	root = 0;
-	numNodes = 0;
+    // Set up the root and the initial number of nodes to 0
+    root = 0;
+    numNodes = 0;
 }
 
 template <class KeyType, class ItemType>
 void DictTree<KeyType, ItemType>::add(KeyType newKey, ItemType newItem)
 {
-	// Use containedInSubtree to determine if a node with newKey exists
-	// If it does, use setValue to set the value to newKey and newItem and return.
-	
-	
+    /
+    
+    TreeNode<KeyType, ItemType>* currNode = root;
 
 
-	// If the root hasn't been set up, make a new node with newKey, newItem
-	// and set equal to this new node and return.
-	TreeNode<KeyType, ItemType>* newNode = new TreeNode<KeyType, ItemType>;
-	newNode->key= newKey;
-	newNode->item = newItem;
-	newNode->left = newNode->right = nullptr;
+    // If the root hasn't been set up, make a new node with newKey, newItem
+    // and set equal to this new node and return.
+    if (root==nullptr) {
+        TreeNode<KeyType, ItemType>* newNode = new TreeNode<KeyType, ItemType>;
+        newNode->key= newKey;
+        newNode->item = newItem;
+        newNode->left = newNode->right = nullptr;
 
-	if (root==nullptr) {
-		root = newNode;
-		numNodes = 1;
-		return;
-	}
+        root = newNode;
+        numNodes = 1;
+        return;
+    }
+    / Use containedInSubtree to determine if a node with newKey exists
+    // If it does, use setValue to set the value to newKey and newItem and return.
+    TreeNode<KeyType, ItemType>* n = containedInSubtree(currNode, newKey);
+    if (n != nullptr){
+        n->item = newItem;
+        return;
+    }
+    else{
+        TreeNode<KeyType, ItemType>* newNode = new TreeNode<KeyType, ItemType>;
+        newNode->key= newKey;
+        newNode->item = newItem;
+        newNode->left = newNode->right = nullptr;
 
-	TreeNode<KeyType, ItemType>* currNode = root;
-	while (currNode) {
-		if (containedInSubtree(currNode, newKey)) {
-			currNode->key = newKey;
-			currNode->item = newItem;
-			return;
-		}
-		else if (newKey < currNode->key) {			// left
-			if (currNode->left == nullptr) {
-				currNode->left = newNode;
-				break;
-			}
-			currNode = currNode->left;
-		}
-		else if (newKey > currNode->key) {	// right
-			if (currNode->right == nullptr) {
-				currNode->right = newNode;
-				break;
-			}
-			currNode = currNode->right;
-		}
-		else {		// Equal
-			delete newNode;		// Didn't use the node, so delete it...
-			return;
-		}
-	}
-	numNodes++;
-	
+        while (currNode) {
+            if (newKey < currNode->key) {            // left
+                if (currNode->left == nullptr) {
+                    currNode->left = newNode;
+                    break;
+                }
+                currNode = currNode->left;
+            }
+            else{    // right
+                if (currNode->right == nullptr) {
+                    currNode->right = newNode;
+                    break;
+                }
+                currNode = currNode->right;
+            }
+        }
+    }
+    
+    numNodes++;
+    
 }
 
 template <class KeyType, class ItemType>
 bool DictTree<KeyType, ItemType>::remove(KeyType key)
 {
-	// This method is done.
-	bool isSuccessful = false;
+    // This method is done.
+    bool isSuccessful = false;
 
-	root = removeFromSubtree(root, key, isSuccessful);
+    root = removeFromSubtree(root, key, isSuccessful);
 
-	if (isSuccessful)
-		numNodes--;
+    if (isSuccessful)
+        numNodes--;
 
-	return isSuccessful;
+    return isSuccessful;
 }
 
 template<class KeyType, class ItemType>
 TreeNode<KeyType, ItemType>* DictTree<KeyType, ItemType>::removeFromSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key, bool& success)
-{
-	// Refer to previous implementation nodes
-	return nullptr;
+==
+    // Refer to previous implementation nodes
+if
+else
+eif
+
+    return nullptr;
 }
 
 /*
@@ -123,94 +130,91 @@ TreeNode<KeyType, ItemType>* DictTree<KeyType, ItemType>::removeFromSubtree(Tree
 template<class KeyType, class ItemType>
 TreeNode<KeyType, ItemType>* DictTree<KeyType, ItemType>::removeNode(TreeNode<KeyType, ItemType>* node)
 {
-	// Refer to previous implementation nodes
-	return nullptr;
+    // Refer to previous implementation nodes
+    return nullptr;
 }
 
 template<class KeyType, class ItemType>
 TreeNode<KeyType, ItemType>* DictTree<KeyType, ItemType>::removeLeftmostNode(TreeNode<KeyType, ItemType>* node, TreeNode<KeyType, ItemType>* successor)
 {
-	// Refer to previous implementation nodes
-	return nullptr;
+    // Refer to previous implementation nodes
+    return nullptr;
 }
 
 template<class KeyType, class ItemType>
 ItemType DictTree<KeyType, ItemType>::getItem(KeyType key)
 {
-	// Use containedInSubtree to find the "item" of the node with the "key"
-	// This should be a single line of code.
-	return nullptr;
+    // Use containedInSubtree to find the "item" of the node with the "key"
+    // This should be a single line of code.
+    return nullptr;
 }
 
 template <class KeyType, class ItemType>
 bool DictTree<KeyType, ItemType>::contains(KeyType key)
 {
-	// Use containedInSubtree to see if a node with the "key" is null.
+    // Use containedInSubtree to see if a node with the "key" is null.
 
-	return false;
+    return false;
 }
 
 template <class KeyType, class ItemType>
 TreeNode<KeyType, ItemType>* DictTree<KeyType, ItemType>::containedInSubtree(TreeNode<KeyType, ItemType>* subTree, KeyType key)
 {
-	// If the subtree is nullptr, then return nullptr
-	if (subTree==nullptr)
-	{
-		return nullptr;
-	}
-	// If the subtree key is equal to key, then return the subtree
-	while (subTree)
-		{
-			if (key==subTree->key)
-			{
-				return subTree;
-			}
+    // If the subtree is nullptr, then return nullptr
+    if (subTree==nullptr)
+    {
+        return nullptr;
+    }
+    // If the subtree key is equal to key, then return the subtree
+    while (subTree)
+        {
+            if (key==subTree->key)
+            {
+                return subTree;
+            }
 
-			// If the key is less than the subTree key
-			//   see if the key is contained in the left subtree
-			if (key < subTree->key)
-			{
-				subTree = subTree->left;
-			}
-			// else
-				//   see if the key is contained in the right subtree
-			else if (key > subTree->key)
-			{
-				subTree = subTree->right;
-			}
-			else
-				return nullptr;
+            // If the key is less than the subTree key
+            //   see if the key is contained in the left subtree
+            if (key < subTree->key)
+            {
+                subTree = subTree->left;
+            }
+            // else
+                //   see if the key is contained in the right subtree
+            else
+            {
+                subTree = subTree->right;
+            }
+                
 
-		}
+        }
 
-
-	
-	
-	
+    return nullptr;
 }
 
 template <class KeyType, class ItemType>
 int DictTree<KeyType, ItemType>::size()
 {
-	// This method is completed.
-	return numNodes;
+    // This method is completed.
+    return numNodes;
 }
 
 template<class KeyType, class ItemType>
 void DictTree<KeyType, ItemType>::traverse(void visit(ItemType&)) const
 {
-	// This method is completed.
-	preorder(visit, root);
+    // This method is completed.
+    preorder(visit, root);
 }
 
 template<class KeyType, class ItemType>
 void DictTree<KeyType, ItemType>::preorder(void visit(ItemType&), TreeNode<KeyType, ItemType>* treePtr) const
 {
-	// Your implementation here...
-	if (treePtr == nullptr)
-		return;
+    // Your implementation here...
+    if (treePtr == nullptr)
+        return;
 
-	preorder( visit, treePtr->left);
-	visit(treePtr->item);
-	preorder(visit, treePtr->right);
+    preorder( visit, treePtr->left);
+    visit(treePtr->item);
+    preorder(visit, treePtr->right);
 }
+
--- HashTable.h
+++ HashTable.h
@@ -9,112 +9,112 @@ template<class KeyType, class ItemType>
 class HashTable
 {
 public:
-	HashTable();
-	HashTable(int numBuckets);
+    HashTable();
+    HashTable(int numBuckets);
 
-	void add(KeyType key, ItemType item);
-	bool remove(KeyType key);
-	ItemType getItem(KeyType key);
-	bool contains(KeyType key);
-	int size();
+    void add(KeyType key, ItemType item);
+    bool remove(KeyType key);
+    ItemType getItem(KeyType key);
+    bool contains(KeyType key);
+    int size();
 
-	void traverse(void visit(ItemType&));
+    void traverse(void visit(ItemType&));
 
 private:
-	const int DEFAULT_BUCKET_NUM = 20;		// How many buckets by default
-	DictTree<KeyType, ItemType>** buckets;
-	int numBuckets;
-	int numElems;							// How many elements stored in entire table
-	int getHashIndex(const KeyType& key);
+    const int DEFAULT_BUCKET_NUM = 20;        // How many buckets by default
+    DictTree<KeyType, ItemType>** buckets;
+    int numBuckets;
+    int numElems;                            // How many elements stored in entire table
+    int getHashIndex(const KeyType& key);
 };
 
 template<class KeyType, class ItemType>
 HashTable<KeyType, ItemType>::HashTable()
 {
-	buckets = new DictTree<KeyType, ItemType>* [DEFAULT_BUCKET_NUM];
-	numBuckets = DEFAULT_BUCKET_NUM;
-	numElems = 0;
+    buckets = new DictTree<KeyType, ItemType>*[DEFAULT_BUCKET_NUM];
+    numBuckets = DEFAULT_BUCKET_NUM;
+    numElems = 0;
 
-	// for each bucket, set the i-th bucket equal to a new DictTree of type <KeyType, ItemType>
+    // for each bucket, set the i-th bucket equal to a new DictTree of type <KeyType, ItemType>
 }
 
 template<class KeyType, class ItemType>
 HashTable<KeyType, ItemType>::HashTable(int numBucks)
 {
-	// See previous constructor.  Use numBucks instead of DEFAULT_BUCKET_NUM.
+    // See previous constructor.  Use numBucks instead of DEFAULT_BUCKET_NUM.
 }
 
 template<class KeyType, class ItemType>
 int HashTable<KeyType, ItemType>::size()
 {
-	return numElems;
+    return numElems;
 }
 
 template<class KeyType, class ItemType>
 void HashTable<KeyType, ItemType>::add(KeyType key, ItemType item)
 {
-	// VERY LITTLE CODE HERE
-	// Get the hash bucket Index
+    // VERY LITTLE CODE HERE
+    // Get the hash bucket Index
 
-	// If the hash table does not already contain key
-	//    increase numElems by one
-
-	// add key,item to the DictTree at a buckets index
+    // If the hash table does not already contain key
+    //    increase numElems by one
+    
+    // add key,item to the DictTree at a buckets index
 }
 
 template<class KeyType, class ItemType>
 bool HashTable<KeyType, ItemType>::remove(KeyType key)
 {
-	// VERY LITTLE CODE HERE
-	// Get the hash Index
+    // VERY LITTLE CODE HERE
+    // Get the hash Index
 
-	// If the hash table contains the key
-	//    decrease numElems by one
-	//    remove the key from buckets[b]
-	//    return true
-	// else
-	//    return false
+    // If the hash table contains the key
+    //    decrease numElems by one
+    //    remove the key from buckets[b]
+    //    return true
+    // else
+    //    return false
 
-	return false;
+    return false;
 }
 
 template<class KeyType, class ItemType>
 ItemType HashTable<KeyType, ItemType>::getItem(KeyType key)
 {
-	// Get the hash bucket Index
-	// return the item by using getItem on the correct bucket index.
-	// (HINT:  Should only be 2 line of code at most.  1 is plenty.)
+    // Get the hash bucket Index
+    // return the item by using getItem on the correct bucket index.
+    // (HINT:  Should only be 2 line of code at most.  1 is plenty.)
 }
 
 template<class KeyType, class ItemType>
 bool HashTable<KeyType, ItemType>::contains(KeyType key)
 {
-	// Get the hash bucket Index
-	// Use contains on the correct bucket index to return the correct true false.
-	// (HINT:  Should only be 2 lines of code at most.  1 is plenty.)
-	return false;
+    // Get the hash bucket Index
+    // Use contains on the correct bucket index to return the correct true false.
+    // (HINT:  Should only be 2 lines of code at most.  1 is plenty.)
+    return false;
 }
 
 template<class KeyType, class ItemType>
-int HashTable<KeyType, ItemType>::getHashIndex(const KeyType& key)
+int HashTable<KeyType, ItemType>::getHashIndex(const KeyType & key)
 {
-	// This is done... No touching!
+    // This is done... No touching!
 
-	// We are creating a hash function type called hashFunction that hashes "KeyType."
-	// First we create an an unordered_map object for our KeyType and ItemType
-	unordered_map<KeyType, ItemType> mapper;
+    // We are creating a hash function type called hashFunction that hashes "KeyType."
+    // First we create an an unordered_map object for our KeyType and ItemType
+    unordered_map<KeyType, ItemType> mapper;
 
-	// Then we call the hash_function method to return the hash function
-	// for the KeyType and assign it to 'hashFunction'
-	typename unordered_map<KeyType, ItemType>::hasher hashFunction = mapper.hash_function();
+    // Then we call the hash_function method to return the hash function
+    // for the KeyType and assign it to 'hashFunction'
+    typename unordered_map<KeyType, ItemType>::hasher hashFunction = mapper.hash_function();
 
-	// static_cast needed since hashFunction returns an unsigned long
-	return static_cast<int>(hashFunction(key) % numBuckets);
+    // static_cast needed since hashFunction returns an unsigned long
+    return static_cast<int>(hashFunction(key) % numBuckets);
 }
 
 
 template<class KeyType, class ItemType>
 void HashTable<KeyType, ItemType>::traverse(void visit(ItemType&))
 {
-	// You should use the traverse function of the DictTree
+    // You should use the traverse function of the DictTree
 }
